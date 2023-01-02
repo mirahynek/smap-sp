ed58fb8a4aeb25f7a081be99a853dcbb41caa4c6
mirahynek <81629314+mirahynek@users.noreply.github.com>
2023-01-02 23:29:27 +0100

    Update projekt.py
    Nastavení a testování kamery

--- projekt.py
+++ projekt.py
@@ -1,10 +1,16 @@
 from gpiozero import MotionSensor
+from picamera import PiCamera
 import time
 
 pir = MotionSensor(4)
+camera = PiCamera()
+filename = "intruder.h264"
 cas = 0
 while True:
     pir.wait_for_motion()
     print(f"Pohyb {cas} ")
     time.sleep(1)
     cas += 1
+    camera.start_recording(filename)
+    pir.wait_for_no_motion()
+    camera.stop_preview()
