60a5748d1f8d94408766178b5367fac96cf446a5
mirahynek <81629314+mirahynek@users.noreply.github.com>
2023-01-14 22:37:52 +0100

    Final
    Finální funkční verze projektu

--- projekt.py
+++ projekt.py
@@ -1,10 +1,43 @@
+#from gpiozero import Button
 from gpiozero import MotionSensor
-import time
-
+from picamera import PiCamera
+import datetime
+from time import sleep
+import smtplib
+from email.message import EmailMessage
+camera = PiCamera()
+#button = Button(5)
 pir = MotionSensor(4)
-cas = 0
+#button.wait_for_press()
 while True:
     pir.wait_for_motion()
-    print(f"Pohyb {cas} ")
-    time.sleep(1)
-    cas += 1
+    #button.wait_for_press()
+    x = datetime.datetime.now()
+    text = x.strftime("%Y-%m-%d_%H-%M-%S0_%a_")
+    cesta = '/home/pi/Desktop/kamera/' + text + 'snímek.png'
+    camera.capture(cesta)
+    print("Snímek OK")
+    #camera.close()
+    sleep(10)
+    filename = text + ".h264"
+    camera.start_recording(filename)
+    sleep(10)    
+    camera.stop_recording()
+    print("Video OK")
+# ověření
+    email_address = "eduraspberry@seznam.cz"
+    email_password = "Meganek5."
+# vytvoř e-mail
+    msg = EmailMessage()
+    msg['Subject'] = "RPi notifikace"
+    msg['From'] = email_address
+    msg['To'] = "mira.hynek@seznam.cz"
+    msg.set_content("Toto je notifikace
+    z RPi " + text)
+# pošli email
+    with smtplib.SMTP_SSL('smtp.seznam.cz', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+
+    print("E-mail odeslán")
+# print(x.strftime("%Y-%m-%d_%H-%M-%S0_%a"))
